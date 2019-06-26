c8524897346624cb9959caa08e82432f9d4b97f8
Joo An Choi <jooanchoi@gmail.com>
2019-06-26 16:23:28 -0400

    final

---
+++ src/main/java/com/revature/Driver.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+import java.io.IOException;
+import com.revature.controller.Controller;
+
+/** 
+ * Create an instance of your controller and launch your application.
+ * 
+ * Try not to have any logic at all on this class.
+ */
+public class Driver {
+
+	public static void main(String[] args) throws IOException {
+		
+		new Controller();
+	}
+}
